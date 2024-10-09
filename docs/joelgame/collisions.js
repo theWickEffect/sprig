fcef791168142d34c756dba6a0e3f5f912f39058
Joel Sheppard <sheppard.joel@gmail.com>
2024-10-09 11:05:28 -0700

    detailed colision checking only for super fast jumps

--- docs/joelgame/collisions.js
+++ docs/joelgame/collisions.js
@@ -4,6 +4,8 @@ import { J3 } from "./joel-game.js";
 export function checkForCollision(start, end, object, marginOfError) {
     if (J3.dist(end, object) <= marginOfError)
         return true;
+    if (J3.dist(start, end) < marginOfError)
+        return false;
     const t = -1 * V3.dot(J3.sub(start, object, true), J3.sub(end, start, true)) / Math.pow(J3.len(J3.sub(end, start, true)), 2);
     if (t < 0 || t > 1.3)
         return false;
